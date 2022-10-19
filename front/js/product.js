2e2c6e6142ee0ed31e7a935d15d2a6145815d10b
Vito Caramia <vcaramia66@gmail.com>
2022-10-18 21:46:38 -0400

    10-18 Milestone 6

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,6 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 console.log(queryString);
 
-
 const productId = urlParams.get("id");
 console.log(productId);
 
@@ -11,21 +10,25 @@ fetch(`http://localhost:3000/api/products/${productId}`)
   .then((response) => response.json())
   .then((product) => toCart(product));
 
-
-
 function toCart(product) {
   const imageHolder = document.querySelector(".item__img");
   const imageTag = document.createElement("img");
   imageTag.setAttribute("src", product.imageUrl);
   imageTag.setAttribute("alt", product.altTxt);
   imageHolder.appendChild(imageTag);
+
+  const productName = document.getElementById("title");
+  productName.innerText = product.name;
+
+  const productDescription = document.getElementById("description");
+  productDescription.innerText = product.description;
+
+  //TODO Insert price
+
+  //TODO Insert color option. Iterate over product colors (for Loop) append to Select tag
 }
-//TODO Insert product name
-//TODO Insert price
-//TODO Insert description
-//TODO Insert color option. Iterate over product colors (for Loop) appent to Select tag
 
 //MILESTONE 7
 //TODO Add event listener to add to cart button
 //TODO create function to handle click event. Use "add to cart"function name
-//TODO add to cart function needs to get, color and quantity selected 
+//TODO add to cart function needs to get, color and quantity selected
--- front/js/script.js
+++ front/js/script.js
@@ -2,59 +2,16 @@
 console.log("hello world");
 fetch("http://localhost:3000/api/products")
   .then((response) => response.json())
-  .then((products) => card(products));
-//create a single article as shown below off of data index 0
-//document.createElement
+  .then((bananas) => card(bananas));
 
-// Create each product section
-
-/* <a href="./product.html?id=42">
-<article>
-  <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1"></img>
-  <h3 class="productName">Kanap name1</h3>
-  <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-</article>
-</a> */
-
-
-// What is this doing?
-
-function createCard(array) {
-  const container = document.getElementById("items");
-  const length = array.length;
-
-  for (let i = 0; i < length; i++) {
-    const card = createCard(array[i]);
-    container.appendChild(card);
-  }
-}
-
-// function createCard(obj) {
-//   const card = document.createElement("section");
-
-//   const name = document.createElement("h2");
-
-//   card.classList.add("card");
-//   name.innerHTML = obj.name;
-
-//   card.appendChild(name);
-
-//   return card;
-// }
 function card(products) {
   console.log(products);
 
-  //TODO Iterate over the products(this is where you create the for loop for the rest of the lines)
-  //Examine each property of the product in the current iteration
-  //Create a new product html element
-  //Create an html element for each product property (only if needed)
-  //Add all the html elments for each property to the new product html element
-
   let section = document.getElementById("items");
 
-  for (let i = 0; i < products.length; i++) {
-    let product = products[i];
-
+  // for (let i = 0; i < products.length; i++) {
+  // let product = products[i];
+  for (let product of products) {
     let a = document.createElement("a");
     a.setAttribute("href", `./product.html?id=${product._id}`);
 
@@ -64,13 +21,11 @@ function card(products) {
     const img = document.createElement("img");
     const name = document.createElement("h3");
 
-    // Scott added
     name.classList.add("productName");
 
     const productDescription = document.createElement("p");
 
     productDescription.classList.add("productDescription");
-
 
     name.innerHTML = product.name;
     productDescription.innerHTML = product.description;
@@ -78,46 +33,13 @@ function card(products) {
     img.setAttribute("src", product.imageUrl);
     img.setAttribute("alt", "product image");
 
-    // document.getElementsByTagName("h2").setAttribute("class", "productName");
-
     card.appendChild(img);
     card.appendChild(name);
     card.appendChild(productDescription);
 
     section.appendChild(a);
   }
-//Why is byClassName ok here?
 
   var cards = document.getElementsByClassName("card");
   console.log(cards);
 }
-
-
-// function productPage() {
-//   alert("hi");
-// }
-
-// productPage();
-
-
-// for (let i = 0; i < cards.length; i++) {
-//   const card = cards[i];
-//   card.addEventListener("click", function () {
-//     alert("Hello World!" + i);
-//   });
-// }
-
-
-
-
-// // Hi there! message at bottom
-// // 1. Create <div> element
-// let div = document.createElement("div");
-
-// // 2. Set its class to "alert"
-// div.className = "alert";
-
-// // 3. Fill it with the content
-// div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
-
-// document.body.append(div);
