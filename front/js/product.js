6a3ed9ed19c6739b00fdb58629bc79793302ab21
Vito Caramia <vcaramia66@gmail.com>
2022-10-20 21:37:16 -0400

    10-20 Product page

--- front/js/product.js
+++ front/js/product.js
@@ -25,10 +25,31 @@ function toCart(product) {
 
   //TODO Insert price
 
-  //TODO Insert color option. Iterate over product colors (for Loop) append to Select tag
-}
+  const productPrice = document.getElementById("price");
+  productPrice.innerText = product.price;
 
-//MILESTONE 7
-//TODO Add event listener to add to cart button
-//TODO create function to handle click event. Use "add to cart"function name
-//TODO add to cart function needs to get, color and quantity selected
+  /*TODO Insert color option -
+  Iterate over product colors (for Loop) append to Select tag*/
+
+  for (let i = 0; i < product.colors.length; i++) {
+    const colorsElement = document.getElementById("colors");
+    const optionElement = document.createElement("option");
+    optionElement.setAttribute("value", product.colors[i]);
+    optionElement.innerText = product.colors[i];
+    colorsElement.appendChild(optionElement);
+  }
+
+  // for (let i = 0; i < product.colors.length; i++) {
+  // for (let color of product.colors) {
+  //   const colorsElement = document.getElementById("colors");
+  //   const optionElement = document.createElement("option");
+  //   optionElement.setAttribute("value", color);
+  //   optionElement.innerText = color;
+  //   colorsElement.appendChild(optionElement);
+  // }
+
+  //MILESTONE 7
+  //TODO Add event listener to add to cart button
+  //TODO create function to handle click event. Use "add to cart"function name
+  //TODO add to cart function needs to get, color and quantity selected
+}
