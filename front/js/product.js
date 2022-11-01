bdb239045f09f1b0b65a3b73e74b0edab0974dd6
Vito Caramia <vcaramia66@gmail.com>
2022-11-01 18:54:36 -0400

    Post Mentor 11/1

--- front/js/product.js
+++ front/js/product.js
@@ -45,41 +45,24 @@ clickAddToCart.addEventListener("click", () => {
   var color = document.getElementById("colors").value;
   console.log(productId);
   console.log(JSON.parse(localStorage.getItem("cart")));
-  //TODO
-  // Add item to cart
-  // note cart items are unique due to ID and color
+
   const cartItem = {
     quantity: quantity,
     color: color,
-    sofaId: productId
-    //TODO Add ID and color
-   
+    sofaId: productId,
   };
 
   // Store in local storage
-  const cartArray = []; // TODO move out of function or you'll have empty cart
-  cartArray.push(cartItem);
-  localStorage.setItem("cart", JSON.stringify(cartArray));
-  //TODO
-  // Scenario #1 Cart doesn't exist or is empty
-  // Scenario #2 Cart exists but item selected is not in it yet
-  // Scenario #3 Check if product has been added to cart (if statement and condtion)
-  //              Increase quantity of existing cart item
+  let cartContents = JSON.parse(localStorage.getItem("cart"));
 
-let cartContents = localStorage.getItem('cartArray');
-localStorage.key('cartArray');
-
-if (cartContents >0){
-  cartItem.quantity+cartArray.quantity;
-}
+  if (cartContents !== null && cartContents.length > 0) {
+    //TODO check to see if product item selected is already in cart with same color
+    //if yes, then increase the quantity (scenario 3)
+    //if no, push the cart item (scenario 2)
+  } else {
+    // Scenario #1 Cart doesn't exist or is empty/Done
+    const cartArray = [];
+    cartArray.push(cartItem);
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+  }
 });
-
-// var newArray [color, quantity];
-
-// local storage
-// getItem(key) - will get the item at the given key value.
-// key(index) - finds the key value at the given index.
-// setItem(key, value) - will allow you to store a key and a value.
-// removeItem(key) - removes the item at the given key value.
-// clear() - removes everything.
-// length - will return you the length of the list of stored items.
